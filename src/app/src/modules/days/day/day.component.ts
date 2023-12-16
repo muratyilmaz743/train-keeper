263c2124af890077e8ad0a874dd50f6fedb3b747
muratyilmaz743 <mrt.yilmaz743@gmail.com>
2023-12-16 13:21:05 +0300

    basic style changes for user friendly

--- src/app/src/modules/days/day/day.component.ts
+++ src/app/src/modules/days/day/day.component.ts
@@ -1,17 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { ModalService } from '../../../shared/modal/modal.service';
 import { ModalComponent } from '../../../shared/modal/modal.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tracker-day',
   standalone: true,
-  imports: [ModalComponent],
+  imports: [ReactiveFormsModule, ModalComponent],
   templateUrl: './day.component.html',
   styleUrl: './day.component.css',
   providers: [ModalService],
 })
 export class DayComponent {
   modal = inject(ModalService);
+
+  addExerciseForm = new FormGroup({
+    exerciseName: new FormControl(''),
+    sets: new FormControl(0),
+    reps: new FormControl(0),
+  });
 
   openModal() {
     this.modal.openModal();
