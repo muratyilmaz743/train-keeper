b2795aa5b4f52d618179ab241f224c20606fbe03
muratyilmaz743 <mrt.yilmaz743@gmail.com>
2023-12-31 11:37:30 +0300

    training records page

--- src/app/src/modules/training-record/training-record.component.ts
+++ src/app/src/modules/training-record/training-record.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ModalService } from '../../shared/modal/modal.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tracker-training-record',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './training-record.component.html',
   styleUrl: './training-record.component.css',
 })
 export class TrainingRecordComponent implements OnInit {
+  public trainingName = '';
   constructor(
     private route: ActivatedRoute,
     public router: Router,
   ) {}
 
   ngOnInit() {
-    const trainingName = this.route.snapshot.paramMap;
-    console.log(trainingName);
+    this.trainingName = this.route.snapshot.paramMap.get('trainingName')!;
   }
+
+  addExerciseForm = new FormGroup({
+    date: new FormControl(''),
+    weight: new FormControl(0),
+  });
 }
