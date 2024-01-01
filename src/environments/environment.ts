fc2c56d591a40344d499bcbfed2077ca13f02724
muratyilmaz743 <mrt.yilmaz743@gmail.com>
2024-01-01 17:27:24 +0300

    firebase init

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCO0PxS9802J9IkgC6R1EWl9HTogC4wIsk',
+  authDomain: 'train-keeper.firebaseapp.com',
+  databaseURL:
+    'https://train-keeper-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'train-keeper',
+  storageBucket: 'train-keeper.appspot.com',
+  messagingSenderId: '480902524535',
+  appId: '1:480902524535:web:9f36c6f9e816166c5de072',
+  measurementId: 'G-2NC3EQZ3MD',
+};
