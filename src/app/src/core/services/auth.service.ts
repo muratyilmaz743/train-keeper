afbabafc5f0e64e22e25abeae3984d903b4dc134
muratyilmaz743 <mrt.yilmaz743@gmail.com>
2024-01-01 17:27:43 +0300

    auth service created

---
+++ src/app/src/core/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { firebaseConfig } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {}
+  app = initializeApp(firebaseConfig);
+
+  auth = getAuth(this.app);
+
+  signUp(email: string, password: string) {
+    createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential.user);
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+      });
+  }
+
+  login(email: string, password: string) {
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential.user);
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+      });
+  }
+}
